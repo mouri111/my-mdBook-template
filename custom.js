b8b86cf15aab30c6ccc4938d97b0062e80a5b8d8
Masato Mouri <13274198+mouri111@users.noreply.github.com>
2020-11-23 02:13:08 +0900

    Use KaTeX

---
+++ custom.js
@@ -0,0 +1,5 @@
+renderMathInElement(document.body, {delimiters: [
+    {left: "\\[", right: "\\]", display: true},
+    {left: "$$", right: "$$", display: true},
+    {left: "$", right: "$", display: false}
+]});
--- src/chapter_1.md
+++ src/chapter_1.md
@@ -1,1 +1,11 @@
 # Chapter 1
+
+## Using KaTeX
+
+\\[
+    H = \frac{1}{\sqrt{2}}
+    \begin{bmatrix}
+        1 & 1 \cr
+        1 & -1
+    \end{bmatrix}
+\\]
